c7d9ead3a62b84e3c0656709379ddb50e727bc11
Stephen Gachoka <stephengachoka57@gmail.com>
2022-10-23 23:56:16 +0300

    initial

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBDGv1tUx0nwFkX1XH2_16g5_Z2Volj51M",
+    authDomain: "trainers-87e51.firebaseapp.com",
+    projectId: "trainers-87e51",
+    storageBucket: "trainers-87e51.appspot.com",
+    messagingSenderId: "364873111307",
+    appId: "1:364873111307:web:b9826bc34dfd722a71340e",
+    measurementId: "G-2V12K5H19B"
+  };
